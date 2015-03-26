57af9fba958cf918c7a7fd827dd35a5d49891e44
onlyindreams <naoya@NAOYA-no-MacBook-Air-2.local>
2015-03-27 00:15:45 +0900

    pyramid practice...

--- PgMaster/pgmaster/views.py
+++ PgMaster/pgmaster/views.py
@@ -1,6 +1,17 @@
 from pyramid.view import view_config
+import os
+import commands
+# import psycopg2
+import datetime
 
 
 @view_config(route_name='home', renderer='templates/mytemplate.pt')
 def my_view(request):
     return {'project': 'PgMaster'}
+
+
+@view_config(route_name='test',renderer='templates/test.pt')
+def test(request):
+    os.chdir("../master")
+    result=commands.getoutput("git checkout REL9_2_STABLE")
+    return dict(test=result)
