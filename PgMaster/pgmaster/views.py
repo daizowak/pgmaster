b7a7e9c4c06f3fd378651b223cffe44930b453b0
naoya <naoya@nes>
2015-03-27 12:39:28 +0900

    pyramid practice

--- PgMaster/pgmaster/views.py
+++ PgMaster/pgmaster/views.py
@@ -13,5 +13,10 @@ def my_view(request):
 @view_config(route_name='test',renderer='templates/test.pt')
 def test(request):
     os.chdir("../master")
+    check = None
+    if '9.2' in request.params:
+	check = request.params['9.2']
+    if '9.1' in request.params:
+        check = request.params['9.1']
     result=commands.getoutput("git checkout REL9_2_STABLE")
-    return dict(test=result)
+    return dict(test=result,myself=request.route_url('test'),check=check)
