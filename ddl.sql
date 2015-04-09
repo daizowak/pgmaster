fb3a405d6f6e34eaf72f4607ec8d8d5bbaf365d8
naoya <naoya@nes>
2015-04-09 18:15:58 +0900

    Add table relatedcommit for commitid relation feature.

--- ddl.sql
+++ ddl.sql
@@ -1,27 +1,36 @@
-drop table _version cascade;
-create table _version
+-- #-- uninstall each tables.
+-- drop table _version cascade;
+-- drop table relatedcommit;
+create table if not exists _version
 (
-logid bigserial primary key,  -- テーブル毎にユニークなプライマリキー
-commitid text unique,         -- commitid (from git log)
-scommitid text unique,          -- (short)commitid ( from git log)
-commitdate date,              -- commitdate (from git log)
-updatetime timestamp default now(), -- 情報を更新した日付
-seclevel text,  -- セキュリティレベル
-reporturl text, -- バグ報告URL
-buglevel text,  -- バグのレベル
-revision text,  -- 当該コミットのリビジョン
-relememo text,  -- 該当リリースノートのメモ
-releurl text,   -- 該当リリースノートのURL
-genre   text,   -- 当該コミットのジャンル
-snote    text,   -- 当該コミットの日本語簡易メモ
-note   text,     -- 当該コミットの日本語メモ
-analysys    text,     -- 当該コミットの分析情報メモ
-keyword   text      -- 検索タグ
+logid bigserial primary key  -- テーブル毎にユニークなプライマリキー
+,commitid text unique         -- commitid (from git log)
+,scommitid text unique          -- (short)commitid ( from git log)
+,commitdate date              -- commitdate (from git log)
+,updatetime timestamp default now() -- 情報を更新した日付
+,seclevel text  -- セキュリティレベル
+,reporturl text -- バグ報告URL
+,buglevel text  -- バグのレベル
+,revision text  -- 当該コミットのリビジョン
+,relememo text  -- 該当リリースノートのメモ
+,releurl text   -- 該当リリースノートのURL
+,genre   text   -- 当該コミットのジャンル
+,snote    text   -- 当該コミットの日本語簡易メモ
+,note   text     -- 当該コミットの日本語メモ
+,analysys    text     -- 当該コミットの分析情報メモ
+,keyword   text      -- 検索タグ
 );
 
-create table REL8_4_STABLE(like _version including all) inherits(_version);
-create table REL9_0_STABLE(like _version including all) inherits(_version);
-create table REL9_1_STABLE(like _version including all) inherits(_version);
-create table REL9_2_STABLE(like _version including all) inherits(_version);
-create table REL9_3_STABLE(like _version including all) inherits(_version);
-create table REL9_4_STABLE(like _version including all) inherits(_version);
+create table if not exists REL8_4_STABLE(like _version including all) inherits(_version);
+create table if not exists REL9_0_STABLE(like _version including all) inherits(_version);
+create table if not exists REL9_1_STABLE(like _version including all) inherits(_version);
+create table if not exists REL9_2_STABLE(like _version including all) inherits(_version);
+create table if not exists REL9_3_STABLE(like _version including all) inherits(_version);
+create table if not exists REL9_4_STABLE(like _version including all) inherits(_version);
+
+create table if not exists RELATEDCOMMIT
+(
+src_commitid text
+,dst_commitid text
+);
+
